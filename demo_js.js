ae04d4a16f9bb8c0db4b9b08b7170d40c9450c61
Olivia Smith <olivia.f.smith@gmail.com>
2019-05-27 09:14:33 -0700

    demo play.js from first lec

--- demo_js.js
+++ demo_js.js
@@ -3,3 +3,23 @@
 
 
 
+"use strict"
+
+function adder(x, y) {
+    return x + y;
+}
+
+
+
+function adding(x, y, z) {
+    var result = x + y;
+
+    if (z == "double") {
+        var results = (x + y) * 2;
+    }
+
+    return result;
+};
+
+
+console.log(adding(5, 6, "double"));
