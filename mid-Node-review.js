89f1e1e0c41a94cd2d03d11b471bab44e96f5e44
Olivia Smith <olivia.f.smith@gmail.com>
2019-06-05 13:17:30 -0700

    adding more features

--- mid-Node-review.js
+++ mid-Node-review.js
@@ -1,1 +1,22 @@
-/* More advanced JS topics from classes, methods, etc */
+/* More advanced JS topics from classes, methods, etc 
+
+from JS OO Lecture https://fellowship.hackbrightacademy.com/materials/ft25a/lectures/js-oo/
+
+Topics:
+- Objects
+- Creating adn using classes
+- Inheritance
+
+*/
+
+
+// 1. In Javascript, dictionaries and Objects are the same thing:
+
+let ezra = new Object();
+
+ezra.name = 'ezra';
+ezra.hunger = 30;
+ezra.mood = 'grump';
+ezra['age'] = 4;
+
+
